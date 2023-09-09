e3e6fda8f41814f422f39220eb85cb4aa5c5df4c
AsmaKacem1 <asma.kacem217@gmail.com>
2023-09-09 16:30:22 +0100

    create function 3

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -5,7 +5,7 @@
 char space[3][3] = { {'1','2','3'},{'4','5','6'},{'7','8','9'} };
 int row, colum;
 char token = 'x';
-bool tie = false;
+bool test = false;
 std::string n1 = "";
 std::string n2 = "";
 
@@ -97,6 +97,24 @@ void functionTwo() {
 	}
 
 };
+
+bool functionThree() 
+{
+	for (int i = 0; i < 3; i++) {
+		if (space[i][0] == space[i][1] && space[i][0] == space[i][2] || space[0][i] == space[1][i] && space[0][i] == space[2][i]) return true;
+	}
+	if (space[0][0] == space[1][1] && space[1][1] == space[2][2] || space[0][2] == space[1][1] && space[1][1] == space[2][0]) return true;
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (space[i][j] != 'x' && space[i][j] != '0') return false;
+		}
+	}
+	test = true;
+	return false;
+
+};
+
 int main() {
 	std::cout << "Enter Player's one name: \n";
 	getline(std::cin, n1);
